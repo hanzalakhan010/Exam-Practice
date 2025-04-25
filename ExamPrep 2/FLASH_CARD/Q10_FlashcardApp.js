2da6177878c2ca05ade5bf2531fc14f2e5a62362
Hanzala Khan <pythondeveloper360@gmail.com>
2025-04-25 11:09:15 +0500

    Add Flashcard App with TypeScript and HTML for creating and managing flashcards

---
+++ ExamPrep 2/FLASH_CARD/Q10_FlashcardApp.js
@@ -0,0 +1,5 @@
+"use strict";
+// Question 10: Flashcard App
+// Create flashcards with question/answer
+// - User can flip card, add new flashcards
+// - TypeScript for structure, DOM manipulation in JS
---
+++ ExamPrep 2/FLASH_CARD/Q10_FlashcardApp.ts
@@ -0,0 +1,4 @@
+// Question 10: Flashcard App
+// Create flashcards with question/answer
+// - User can flip card, add new flashcards
+// - TypeScript for structure, DOM manipulation in JS
