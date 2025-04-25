6c9a25ff7d2045691864d2769d780a05d7f293c0
Hanzala Khan <pythondeveloper360@gmail.com>
2025-04-25 11:09:57 +0500

    Add TechBlog interface for creating, editing, and deleting blog posts with localStorage support

---
+++ ExamPrep 2/TechBlog/Q3_TechBlog.js
@@ -0,0 +1,7 @@
+"use strict";
+// Question 3: TechBlog
+// Build a blogging interface with the following:
+// - Create, edit, delete blog posts
+// - Store posts in localStorage
+// - Use TypeScript interface for BlogPost
+// - Render posts in a card layout
